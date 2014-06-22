64237f7f441f0849f02e5582bef7ea35e217e0eb
Cameron Yule <cameron@cameronyule.com>
2014-06-22 13:00:39 +0100

    Adding HTML minification task to build process.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,13 +7,13 @@ module.exports = function(grunt) {
 
   grunt.loadTasks('grunt');
 
-  grunt.registerTask('build',
-    'Build site files for testing or deployment.',
-    ['clean:build', 'requirejs', 'cssmin', 'compress', 'copy']);
-
   grunt.registerTask('photos',
     'Resize, optimise and build database of your source images.',
     ['clean:photos', 'responsive_images', 'imageoptim', 'photo_database']);
+
+  grunt.registerTask('build',
+    'Build optimised version of the site for deployment.',
+    ['clean:build', 'requirejs', 'cssmin', 'compress', 'copy', 'targethtml', 'htmlmin']);
 
   grunt.registerTask('default', ['build']);
 };
